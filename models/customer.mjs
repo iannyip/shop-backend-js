328a926a99e9551bc7a2077b7d7e0c947d2dc0d8
iannyip <ianyipsh@gmail.com>
2022-05-15 23:45:45 +0800

    added customer model

---
+++ models/customer.mjs
@@ -0,0 +1,29 @@
+export default function initProductModel(sequelize, DataTypes) {
+  return sequelize.define(
+    "customer",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    { underscored: true }
+  );
+}
