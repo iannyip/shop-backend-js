7c72e0ebc36d8dc00eb880791bb0228bb45c7289
iannyip <ianyipsh@gmail.com>
2022-05-16 00:46:20 +0800

    created template to insert data

--- seeders/20220515163732-seed-data.js
+++ seeders/20220515163732-seed-data.js
@@ -2,23 +2,34 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
+    const customerList = [];
+    const productList = [];
+    const orderList = [];
+    const orderProductList = [];
+
+    // insert into tables
+    let customers = await queryInterface.bulkInsert("customers", customerList, {
+      returning: true,
+    });
+    let products = await queryInterface.bulkInsert("products", productList, {
+      returning: true,
+    });
+    let orders = await queryInterface.bulkInsert("orders", orderList, {
+      returning: true,
+    });
+    let orderProducts = await queryInterface.bulkInsert(
+      "order_products",
+      orderProductList,
+      {
+        returning: true,
+      }
+    );
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete("order_products", null);
+    await queryInterface.bulkDelete("orders", null);
+    await queryInterface.bulkDelete("products", null);
+    await queryInterface.bulkDelete("customers", null);
   },
 };
