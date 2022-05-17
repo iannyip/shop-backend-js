198457e3f6d75ab29b3708257387af61228a232e
iannyip <ianyipsh@gmail.com>
2022-05-17 23:12:25 +0800

    added boilerplate express root file index.mjs

---
+++ index.mjs
@@ -0,0 +1,22 @@
+// 1a. Import NPM libraries
+import express from "express";
+import cookieParser from "cookie-parser";
+import methodOverride from "method-override";
+
+// 1b. Import bindRoutes function
+import bindRoutes from "./routes.mjs";
+
+// 2. Variables
+
+// 3. Initialise Express
+const app = express();
+
+// 4. All the additional middleware we want to add
+
+// 5. Bind route definitions to the instance of Express (app)
+bindRoutes(app);
+
+// 6. Set your instance of express to listen on the given port
+console.log("starting server...");
+const PORT = process.env.PORT || 3004;
+app.listen(PORT);
