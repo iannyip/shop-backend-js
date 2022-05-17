31607872eee8ce30ea5f008b3b0aea5b4717a896
iannyip <ianyipsh@gmail.com>
2022-05-17 23:03:50 +0800

    added comments for steps to models/index.mjs

--- models/index.mjs
+++ models/index.mjs
@@ -1,11 +1,16 @@
+// 1. Import NPM libraries
 import sequelizePackage from "sequelize";
+
+// 2. Import config for database. We use this to initialize a Sequelize object
 import allConfig from "../config/config.js";
 
+// 3. Import models for each table
 import initorderProductModel from "./orderProduct.mjs";
 import initProductModel from "./product.mjs";
 import initCustomerModel from "./customer.mjs";
 import initOrderModel from "./order.mjs";
 
+// 4. Initialize a Sequelize object
 const { Sequelize } = sequelizePackage;
 const env = process.env.NODE_ENV || "development";
 const config = allConfig[env];
@@ -18,7 +23,7 @@ let sequelize = new Sequelize(
   config
 );
 
-// Here we actually define the attributes of the db object
+// 5. Initialize an object for each Model. Store as attributes of the db object
 db.Product = initProductModel(sequelize, Sequelize.DataTypes);
 db.Order = initOrderModel(sequelize, Sequelize.DataTypes);
 db.Customer = initCustomerModel(sequelize, Sequelize.DataTypes);
@@ -26,7 +31,7 @@ db.orderProduct = initorderProductModel(sequelize, Sequelize.DataTypes);
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-// Associations
+// 6. Handle Associations between db.objects.
 db.Order.belongsToMany(db.Product, { through: db.orderProduct });
 db.Product.belongsToMany(db.Order, { through: db.orderProduct });
 db.Order.hasMany(db.orderProduct);
@@ -34,5 +39,5 @@ db.orderProduct.belongsTo(db.Order);
 db.Product.hasMany(db.orderProduct);
 db.orderProduct.belongsTo(db.Product);
 
-// export an OBJECT
+// 7. export db as an OBJECT
 export default db;
