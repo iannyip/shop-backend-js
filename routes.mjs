527487d71d451ef845898293c7c7dd575d487e1e
iannyip <ianyipsh@gmail.com>
2022-05-18 22:59:09 +0800

    updated routes for productController routes

--- routes.mjs
+++ routes.mjs
@@ -2,12 +2,18 @@
 import db from "./models/index.mjs";
 
 // 2. Import controllers from controllers directory
+import initProductController from "./controllers/products.mjs";
 
 // 3. Export function that returns bunch of routes
 export default function bindRoutes(app) {
   // 3a. initialise controllers
+  const productController = initProductController(db);
+
   // 3b. Setup routes
   app.get("/", (request, response) => {
     response.send("you are in the root directory");
   });
+
+  app.get("/products", productController.index);
+  app.get("/products/:id", productController.show);
 }
