7311f972ca2fa87fd05e4d5ca89dc2fc2a5c2eed
iannyip <ianyipsh@gmail.com>
2022-06-12 19:26:52 +0800

    Experimented with npm scripts and prestarts

---
+++ prestart.js
@@ -0,0 +1,11 @@
+// The purpose of this file is to explore NPM scripts
+// See that we are running this file with:
+// -- prestart script: together with `npm start`
+// -- custom script: npm run kkj
+// https://docs.npmjs.com/cli/v8/using-npm/scripts
+
+console.log("\n============");
+console.log("============ \n");
+console.log("RUNNING PRESTART SCRIPT prestart from prestart.js");
+console.log("\n============");
+console.log("============\n");
